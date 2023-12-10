e500d79b7938832c0b6e8e0165f97634b7fcf875
HanYolo <simon.herzog@protonmail.ch>
2023-12-10 17:38:40 +0100

    advent10 part 1 done

--- advent10.py
+++ advent10.py
@@ -1,3 +1,5 @@
+import re
+
 with open("advent10.data", "r") as file:
     lines = [line.strip("\n") for line in file.readlines()]
 
@@ -9,83 +11,44 @@ for row, line in enumerate(lines):
 
 prev = start.copy()
 pos = start.copy()
-count = 0
+pos["row"] -= 1
 
-
-def move():
-    global prev
-    global pos
-    global count
-    next = pos.copy()
-    match lines[pos["row"]["col"]]:
-        case "|":
-            if pos["row"] - 1 != prev["row"]:
-                next["row"] -= 1
-            else:
-                next["row"] += 1
-        case "-":
-            if pos["col"] - 1 != prev["col"]:
-                next["col"] -= 1
-            else:
-                next["col"] += 1
-        case "L":
-            if pos["row"] - 1 != prev["row"]:
-                next["row"] -= 1
-            else:
-                next["col"] += 1
-
-    prev = pos
-    pos = next
-    count += 1
-    print(pos)
-
-    for row in range(max(pos["row"] - 1, 0), min(pos["row"] + 2, len(lines))):
-        for col in range(max(pos["col"] - 1, 0), min(pos["col"] + 2, len(lines[0]))):
-            found = False
-            match lines[row][col]:
-                case "|":
-                    if col == pos["col"]:
-                        found = True
-                case "-":
-                    if row == pos["row"]:
-                        found = True
-                case "L":
-                    if (row == pos["row"] and col + 1 == pos["col"]) or (
-                        row - 1 == pos["row"] and col == pos["col"]
-                    ):
-                        found = True
-                case "J":
-                    if (row == pos["row"] and col - 1 == pos["col"]) or (
-                        row - 1 == pos["row"] and col == pos["col"]
-                    ):
-                        found = True
-                case "7":
-                    if (row == pos["row"] and col - 1 == pos["col"]) or (
-                        row + 1 == pos["row"] and col == pos["col"]
-                    ):
-                        found = True
-                case "F":
-                    if (row == pos["row"] and col + 1 == pos["col"]) or (
-                        row + 1 == pos["row"] and col == pos["col"]
-                    ):
-                        found = True
-                case "S":
-                    if row == pos["row"] or col == pos["col"]:
-                        found = True
-
-            if found:
-                next = {"row": row, "col": col}
-                if next != prev and next != pos:
-                    prev = pos
-                    pos = next
-                    count += 1
-                    print(pos)
-                    return
-
-
-print(start)
-move()
+count = 1
 while pos != start:
-    move()
+        next = pos.copy()
+        match lines[pos["row"]][pos["col"]]:
+            case "|":
+                if pos["row"] - 1 != prev["row"]:
+                    next["row"] -= 1
+                else:
+                    next["row"] += 1
+            case "-":
+                if pos["col"] - 1 != prev["col"]:
+                    next["col"] -= 1
+                else:
+                    next["col"] += 1
+            case "L":
+                if pos["row"] - 1 != prev["row"]:
+                    next["row"] -= 1
+                else:
+                    next["col"] += 1
+            case "J":
+                if pos["row"] - 1 != prev["row"]:
+                    next["row"] -= 1
+                else:
+                    next["col"] -= 1
+            case "7":
+                if pos["row"] + 1 != prev["row"]:
+                    next["row"] += 1
+                else:
+                    next["col"] -= 1
+            case "F":
+                if pos["row"] + 1 != prev["row"]:
+                    next["row"] += 1
+                else:
+                    next["col"] += 1
+        prev = pos
+        pos = next
+        count += 1
 
 print(count // 2)
