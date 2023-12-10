8b9567e2607d670fbf42d7904b68b09b3cb5a55b
HanYolo <simon.herzog@protonmail.ch>
2023-12-10 22:08:18 +0100

    advent10 2 part start

--- advent10.py
+++ advent10.py
@@ -13,15 +13,60 @@ prev = start.copy()
 pos = start.copy()
 pos["row"] -= 1
 
-count = 1
+# count = 1
+# while pos != start:
+#         next = pos.copy()
+#         match lines[pos["row"]][pos["col"]]:
+#             case "|":
+#                 if pos["row"] - 1 != prev["row"]:
+#                     next["row"] -= 1
+#                 else:
+#                     next["row"] += 1
+#             case "-":
+#                 if pos["col"] - 1 != prev["col"]:
+#                     next["col"] -= 1
+#                 else:
+#                     next["col"] += 1
+#             case "L":
+#                 if pos["row"] - 1 != prev["row"]:
+#                     next["row"] -= 1
+#                 else:
+#                     next["col"] += 1
+#             case "J":
+#                 if pos["row"] - 1 != prev["row"]:
+#                     next["row"] -= 1
+#                 else:
+#                     next["col"] -= 1
+#             case "7":
+#                 if pos["row"] + 1 != prev["row"]:
+#                     next["row"] += 1
+#                 else:
+#                     next["col"] -= 1
+#             case "F":
+#                 if pos["row"] + 1 != prev["row"]:
+#                     next["row"] += 1
+#                 else:
+#                     next["col"] += 1
+#         prev = pos
+#         pos = next
+#         count += 1
+# 
+# print(count // 2)
+
+inner = "R"
+marks = [[0] * len(lines[0]) for _ in range(len(lines))]
+marks[start["row"]][start["col"]] = 3
 while pos != start:
         next = pos.copy()
+        marks[pos["row"]][pos["col"]] = 3
         match lines[pos["row"]][pos["col"]]:
             case "|":
                 if pos["row"] - 1 != prev["row"]:
                     next["row"] -= 1
                 else:
                     next["row"] += 1
+                if inner == "R" and 
+                        
             case "-":
                 if pos["col"] - 1 != prev["col"]:
                     next["col"] -= 1
@@ -49,6 +94,9 @@ while pos != start:
                     next["col"] += 1
         prev = pos
         pos = next
-        count += 1
 
-print(count // 2)
+with open("advent10.out", "w") as out:
+    for mark in marks:
+        for m in mark:
+            out.write(str(m))
+        out.write("\n")
