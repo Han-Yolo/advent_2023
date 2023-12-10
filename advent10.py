cdd069bd352281cba0d49a52f4bf09e7f7f1a355
Han-Yolo <simon.herzog@protonmail.ch>
2023-12-10 22:41:04 +0100

    advent10 part 2 done

--- advent10.py
+++ advent10.py
@@ -50,53 +50,154 @@ pos["row"] -= 1
 #         prev = pos
 #         pos = next
 #         count += 1
-# 
+#
 # print(count // 2)
 
-inner = "R"
+
 marks = [[0] * len(lines[0]) for _ in range(len(lines))]
 marks[start["row"]][start["col"]] = 3
-while pos != start:
-        next = pos.copy()
-        marks[pos["row"]][pos["col"]] = 3
-        match lines[pos["row"]][pos["col"]]:
-            case "|":
-                if pos["row"] - 1 != prev["row"]:
-                    next["row"] -= 1
-                else:
-                    next["row"] += 1
-                if inner == "R" and 
-                        
-            case "-":
-                if pos["col"] - 1 != prev["col"]:
-                    next["col"] -= 1
-                else:
-                    next["col"] += 1
-            case "L":
-                if pos["row"] - 1 != prev["row"]:
-                    next["row"] -= 1
-                else:
-                    next["col"] += 1
-            case "J":
-                if pos["row"] - 1 != prev["row"]:
-                    next["row"] -= 1
-                else:
-                    next["col"] -= 1
-            case "7":
-                if pos["row"] + 1 != prev["row"]:
-                    next["row"] += 1
-                else:
-                    next["col"] -= 1
-            case "F":
-                if pos["row"] + 1 != prev["row"]:
-                    next["row"] += 1
-                else:
-                    next["col"] += 1
-        prev = pos
-        pos = next
 
-with open("advent10.out", "w") as out:
-    for mark in marks:
-        for m in mark:
-            out.write(str(m))
-        out.write("\n")
+
+def setMark(row, col):
+    global marks
+    if row >= 0 and row < len(lines) and col >= 0 and col < len(lines[0]):
+        if marks[row][col] == 0:
+            marks[row][col] = 1
+
+
+inner = "L"
+setMark(start["row"], start["col"] - 1)
+
+while pos != start:
+    next = pos.copy()
+    marks[pos["row"]][pos["col"]] = 3
+    match lines[pos["row"]][pos["col"]]:
+        case "|":
+            if pos["row"] - 1 != prev["row"]:
+                next["row"] -= 1
+            else:
+                next["row"] += 1
+
+            if inner == "R":
+                setMark(pos["row"], pos["col"] + 1)
+            elif inner == "L":
+                setMark(pos["row"], pos["col"] - 1)
+
+        case "-":
+            if pos["col"] - 1 != prev["col"]:
+                next["col"] -= 1
+            else:
+                next["col"] += 1
+
+            if inner == "R":
+                setMark(pos["row"] + 1, pos["col"])
+            elif inner == "L":
+                setMark(pos["row"] - 1, pos["col"])
+
+        case "L":
+            if pos["row"] - 1 != prev["row"]:
+                next["row"] -= 1
+            else:
+                next["col"] += 1
+
+            match inner:
+                case "R":
+                    inner = "U"
+                case "U":
+                    inner = "R"
+                case "L":
+                    inner = "D"
+                case "D":
+                    inner = "L"
+
+            if inner == "L" or inner == "D":
+                setMark(pos["row"], pos["col"] - 1)
+                setMark(pos["row"] + 1, pos["col"] - 1)
+                setMark(pos["row"] + 1, pos["col"])
+
+        case "J":
+            if pos["row"] - 1 != prev["row"]:
+                next["row"] -= 1
+            else:
+                next["col"] -= 1
+
+            match inner:
+                case "R":
+                    inner = "D"
+                case "D":
+                    inner = "R"
+                case "L":
+                    inner = "U"
+                case "U":
+                    inner = "L"
+
+            if inner == "R" or inner == "D":
+                setMark(pos["row"], pos["col"] + 1)
+                setMark(pos["row"] + 1, pos["col"] + 1)
+                setMark(pos["row"] + 1, pos["col"])
+
+        case "7":
+            if pos["row"] + 1 != prev["row"]:
+                next["row"] += 1
+            else:
+                next["col"] -= 1
+
+            match inner:
+                case "U":
+                    inner = "R"
+                case "R":
+                    inner = "U"
+                case "D":
+                    inner = "L"
+                case "L":
+                    inner = "D"
+
+            if inner == "U" or inner == "R":
+                setMark(pos["row"] - 1, pos["col"])
+                setMark(pos["row"] - 1, pos["col"] + 1)
+                setMark(pos["row"], pos["col"] + 1)
+
+        case "F":
+            if pos["row"] + 1 != prev["row"]:
+                next["row"] += 1
+            else:
+                next["col"] += 1
+
+            match inner:
+                case "U":
+                    inner = "L"
+                case "L":
+                    inner = "U"
+                case "D":
+                    inner = "R"
+                case "R":
+                    inner = "D"
+
+            if inner == "U" or inner == "L":
+                setMark(pos["row"] - 1, pos["col"])
+                setMark(pos["row"] - 1, pos["col"] - 1)
+                setMark(pos["row"], pos["col"] - 1)
+
+    prev = pos
+    pos = next
+
+count = 0
+for line in marks:
+    active = False
+    for m in line:
+        if m == 1:
+            active = True
+            count += 1
+        elif m == 0 and active:
+            count += 1
+        else:
+            active = False
+
+print(count)
+
+
+# with open("advent10.out", "w") as out:
+#     for mark in marks:
+#         for m in mark:
+#             out.write(str(m))
+#         out.write("\n")
